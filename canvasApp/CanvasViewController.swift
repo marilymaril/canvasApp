912190931ed8494de841e53fffb14ab8e043f9eb
marilymaril <mflorek@csumb.edu>
2018-10-23 17:53:16 -0700

    Fixed the tray view panning gesture and added a drag and drop to the emoji

--- canvasApp/CanvasViewController.swift
+++ canvasApp/CanvasViewController.swift
@@ -14,6 +14,8 @@ class CanvasViewController: UIViewController {
     var trayDownOffset: CGFloat! = nil
     var trayUp: CGPoint!
     var trayDown: CGPoint!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
+    var newlyCreatedFace: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +25,12 @@ class CanvasViewController: UIViewController {
         trayDownOffset = 160
         trayUp = trayView.center // The initial position of the tray
         trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset) // The position of the tray transposed down
+        
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didPan(sender:)))
+        
+        // Attach it to a view of your choice. If it's a UIImageView, remember to enable user interaction
+        newlyCreatedFace.isUserInteractionEnabled = true
+        newlyCreatedFace.addGestureRecognizer(panGestureRecognizer)
     }
     
     @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
@@ -45,5 +53,37 @@ class CanvasViewController: UIViewController {
             }
         }
     }
+    
+    @IBAction func didPanFace(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            let imageView = sender.view as! UIImageView
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            view.addSubview(newlyCreatedFace)
+            newlyCreatedFace.center = imageView.center
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+        } else if sender.state == .changed {
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+
+        } else if sender.state == .ended {
+//            if velocity.y > 0 {
+//                UIView.animate(withDuration: 0.3) {
+//                    self.trayView.center = self.trayDown
+//                }
+//            } else {
+//                UIView.animate(withDuration: 0.3) {
+//                    self.trayView.center = self.trayUp
+//                }
+//            }
+        }
+    }
+    
+    func didPan(_ sender: UIPanGestureRecognizer) {
+        
+    }
+    
+    
 
 }
